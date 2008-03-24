1c1825499ed3b058cedb84df9b87777323dd22a9
mkr <mkr@opencascade.com>
2008-03-24 11:44:25 +0000

    Qt4 porting.

---
+++ src/GHS3DPlugin_icons.ts
@@ -0,0 +1,10 @@
+<!DOCTYPE TS>
+<TS version="1.1" >
+    <context>
+        <name>@default</name>
+        <message>
+            <source>ICON_SMESH_TREE_ALGO_GHS3D_3D</source>
+            <translation>mesh_tree_algo_tetra.png</translation>
+        </message>
+    </context>
+</TS>
